462591bb453e08fe0f305204e77d543298af8d18
jmrenouard <jmrenouard@gmail.com>
2026-08-11 14:55:41 +0200

    feat: Add startup scripts in scripts/ directory

--- scripts/start_postgresql.sh
+++ scripts/start_postgresql.sh
@@ -4,5 +4,5 @@ set -e
 # Start SSHD in the background
 /usr/sbin/sshd 2>/dev/null || true
 
-# Start MySQL using official entrypoint
-exec docker-entrypoint.sh mysqld
+# Start PostgreSQL using official entrypoint
+exec docker-entrypoint.sh postgres
