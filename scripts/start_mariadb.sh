ac8ab5da7c5367b2256cbf7cd7f239af86a949e6
jmrenouard <jmrenouard@gmail.com>
2026-08-11 14:55:34 +0200

    feat: Add startup scripts in scripts/ directory

--- scripts/start_mariadb.sh
+++ scripts/start_mariadb.sh
@@ -4,5 +4,5 @@ set -e
 # Start SSHD in the background
 /usr/sbin/sshd 2>/dev/null || true
 
-# Start MySQL using official entrypoint
-exec docker-entrypoint.sh mysqld
+# Start MariaDB using official entrypoint
+exec docker-entrypoint.sh mariadbd
